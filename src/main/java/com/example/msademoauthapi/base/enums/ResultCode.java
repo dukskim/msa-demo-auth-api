efab822983a0cff98f21ad169c56d5b98fac019f
kdhm <test@test.com>
2025-05-14 20:07:24 +0900

    초기설정중

---
+++ src/main/java/com/example/msademoauthapi/base/enums/ResultCode.java
@@ -0,0 +1,5 @@
+package com.example.msademoauthapi.base.enums;
+
+public enum ResultCode {
+    SUCCESS, ERROR
+}
---
+++ src/test/java/com/example/msademoauthapi/MsaDemoAuthApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.msademoauthapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsaDemoAuthApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
